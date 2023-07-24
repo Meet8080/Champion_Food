beacb7f1123d4b85e6f1ac4c5c34c2d5f36b092c
Meet8080 <patelarjun687@gmail.com>
2023-07-23 23:10:39 -0400

    added pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div className="card" style={{"width": "18rem"}}>
+      <div className="card mt-3" style={{"width": "18rem","maxHeight":"360px"}}>
         <img src="..." className="card-img-top" alt="..." />
         <div className="card-body">
           <p className="card-text">
